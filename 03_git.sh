f19b6794d7f182e4b24faf96c2ad79545638c99f
Daniel Schaaff <dschaaff@knuedge.com>
2017-05-11 14:10:32 -0700

    remove unecessary line

--- 03_git.sh
+++ 03_git.sh
@@ -4,7 +4,7 @@
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
 fi
-source /usr/local/etc/bash_completion.d/git-prompt.sh
+#source /usr/local/etc/bash_completion.d/git-prompt.sh
 GIT_PS1_SHOWDIRTYSTATE=true
 #export PS1='[\u@mbp \w$(__git_ps1)]\$ '
 
